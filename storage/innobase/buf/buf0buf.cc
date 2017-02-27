831a3f0ff4f783e01fda102fd2d62d964809b28b
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2017-02-27 08:34:11 +0200

    Fix bug 85205 / LP TODO (A follow-up fix for buffer pool mutex split patch might be suboptimal, commit 1)
    Almost completely revert upstream commit [1]. It tries to fix a race
    condition between buffer pool-fixing the block and evicting it, resulting
    in a fixed block being evicted. However, the change appears to be bogus:
    - there is no need to add protection to unfixing;
    - in buf_page_get_zip and buf_page_try_get_func, fixing is serialized
      with eviction by the hash;
    - a new critical section in buf_page_get_gen to protect
      buf_page_get_io_fix_unlocked call is not about buffer pool fixing
      but about I/O-fixing, which is different. This issue should be
      re-analyzed if valid.
    
    This only leaves a vaid bit in an UNIV_IBUF_DEBUG code in
    buf_page_get_gen. Thus revert [1], except for the UNIV_IBUF_DEBUG
    code.
    
    commit 65649a16ce9412f7d9286e7e6ad6c2de5769be25
    Author: Shaohua Wang <shaohua.wang@oracle.com>
    Date:   Thu Apr 21 13:34:00 2016 +0800
    
        BUG#23067038 ASSERTION FAILURE: BUF0BUF.CC:2861:BUF_PAGE_IN_FILE(BPAGE)
                     LEADS TO CORRUPT DATA
    
        It's a regression of wl#8423 InnoDB: Split the buffer pool mutex,
        in which we removed block mutex protection for buf_fix_count.
    
        The assertion happens when one thread fixed a dirty block but the
        other thread flushed the block and moved the page from LRU list to
        free list, because it saw buf_fix_count is 0, other than 1.
    
        The solution is holding block mutex when buf fix and unfix.
    
        Reviewed-by: Debarun Banerjee <debarun.banerjee@oracle.com>
        RB: 12456

--- storage/innobase/buf/buf0buf.cc
+++ storage/innobase/buf/buf0buf.cc
@@ -3114,12 +3114,12 @@ err_exit:
 			goto lookup;
 		}
 
+		buf_block_buf_fix_inc((buf_block_t*) bpage,
+				      __FILE__, __LINE__);
+
 		block_mutex = &((buf_block_t*) bpage)->mutex;
 
 		mutex_enter(block_mutex);
-
-		buf_block_buf_fix_inc((buf_block_t*) bpage,
-				      __FILE__, __LINE__);
 
 		goto got_block;
 	}
@@ -3635,12 +3635,9 @@ got_block:
 	if (mode == BUF_GET_IF_IN_POOL || mode == BUF_PEEK_IF_IN_POOL) {
 
 		buf_page_t*	fix_page = &fix_block->page;
-		BPageMutex*	fix_mutex = buf_page_get_mutex(fix_page);
-
-		mutex_enter(fix_mutex);
+		os_rmb;
 		const bool	must_read
 		     = (buf_page_get_io_fix_unlocked(fix_page) == BUF_IO_READ);
-		mutex_exit(fix_mutex);
 
 		if (must_read) {
 			/* The page is being read to buffer pool,
@@ -4100,9 +4097,7 @@ buf_page_optimistic_get(
 	}
 
 	if (!success) {
-		buf_page_mutex_enter(block);
 		buf_block_buf_fix_dec(block);
-		buf_page_mutex_exit(block);
 
 		return(FALSE);
 	}
@@ -4117,9 +4112,7 @@ buf_page_optimistic_get(
 			rw_lock_x_unlock(&block->lock);
 		}
 
-		buf_page_mutex_enter(block);
 		buf_block_buf_fix_dec(block);
-		buf_page_mutex_exit(block);
 
 		return(FALSE);
 	}
@@ -4225,9 +4218,7 @@ buf_page_get_known_nowait(
 	}
 
 	if (!success) {
-		buf_page_mutex_enter(block);
 		buf_block_buf_fix_dec(block);
-		buf_page_mutex_exit(block);
 
 		return(FALSE);
 	}
@@ -4297,16 +4288,16 @@ buf_page_try_get_func(
 
 	ut_ad(!buf_pool_watch_is_sentinel(buf_pool, &block->page));
 
-	buf_page_mutex_enter(block);
+	buf_block_buf_fix_inc(block, file, line);
+
 	rw_lock_s_unlock(hash_lock);
 
 #if defined UNIV_DEBUG || defined UNIV_BUF_DEBUG
+	buf_page_mutex_enter(block);
 	ut_a(buf_block_get_state(block) == BUF_BLOCK_FILE_PAGE);
 	ut_a(page_id.equals_to(block->page.id));
-#endif /* UNIV_DEBUG || UNIV_BUF_DEBUG */
-
-	buf_block_buf_fix_inc(block, file, line);
 	buf_page_mutex_exit(block);
+#endif /* UNIV_DEBUG || UNIV_BUF_DEBUG */
 
 	mtr_memo_type_t	fix_type = MTR_MEMO_PAGE_S_FIX;
 	success = rw_lock_s_lock_nowait(&block->lock, file, line);
@@ -4322,9 +4313,7 @@ buf_page_try_get_func(
 	}
 
 	if (!success) {
-		buf_page_mutex_enter(block);
 		buf_block_buf_fix_dec(block);
-		buf_page_mutex_exit(block);
 
 		return(NULL);
 	}
