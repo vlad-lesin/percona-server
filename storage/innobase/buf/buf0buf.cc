6f223026951738cf98f0d90dc46a9ea2f7376be3
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2017-02-27 08:45:25 +0200

    Fix bug 85208 / PS-3755 (A follow-up fix for buffer pool mutex split patch might be suboptimal, commit 2)
    Almost completely revert upstream commit [1]. It fixes the same issue as
    our 5.7 fix for bug 1664280. However, the upstream commit is needlessly
    pessimistic in locking. Thus revert it, except for the invariant checking
    changes, in preparation of applying our fix.
    
    [1]:
    
    commit cbc524750c486e9fde8266439979476964917bbb
    Author: Shaohua Wang <shaohua.wang@oracle.com>
    Date:   Wed Jun 8 10:29:10 2016 +0800
    
        BUG#23067038 ASSERTION FAILURE: BUF0BUF.CC:2861:BUF_PAGE_IN_FILE(BPAGE)
                     LEADS TO CORRUPT DATA
    
        It's a regression of wl#8423 InnoDB: Split the buffer pool mutex.
    
        There is a race: Thread 1, we set buf_fix_count to 0 in buf_page_init().
        Thread 2, we decrease buf_fix_count to 0xffffffff in buf_page_optimistic_get().
        Thread 2, we increase buf_fix_count to 0 in buf_page_get_gen(). Thread 3,
        we evict the page from LRU list. Thread 2, assert fails: buf_page_in_file().
    
        The root cause is missing block mutex protection for buf_page_init().
    
        Reviewed-by: Debarun Banerjee <debarun.banerjee@oracle.com>
        RB: 12456

--- storage/innobase/buf/buf0buf.cc
+++ storage/innobase/buf/buf0buf.cc
@@ -4072,7 +4072,7 @@ static void buf_page_init(buf_pool_t *buf_pool, const page_id_t &page_id,
 
   ut_ad(buf_pool == buf_pool_get(page_id));
 
-  ut_ad(mutex_own(buf_page_get_mutex(&block->page)));
+  ut_ad(!mutex_own(buf_page_get_mutex(&block->page)));
   ut_a(buf_block_get_state(block) != BUF_BLOCK_FILE_PAGE);
 
   ut_ad(rw_lock_own(buf_page_hash_lock_get(buf_pool, page_id), RW_LOCK_X));
@@ -4238,9 +4238,9 @@ buf_page_t *buf_page_init_for_read(dberr_t *err, ulint mode,
 
     ut_ad(buf_pool_from_bpage(bpage) == buf_pool);
 
-    buf_page_mutex_enter(block);
-
     buf_page_init(buf_pool, page_id, page_size, block);
+
+    buf_page_mutex_enter(block);
 
     /* Note: We are using the hash_lock for protection. This is
     safe because no other thread can lookup the block from the
@@ -4404,9 +4404,9 @@ buf_block_t *buf_page_create(const page_id_t &page_id,
 
   block = free_block;
 
-  buf_page_mutex_enter(block);
-
   buf_page_init(buf_pool, page_id, page_size, block);
+
+  buf_page_mutex_enter(block);
 
   rw_lock_x_unlock(hash_lock);
 
