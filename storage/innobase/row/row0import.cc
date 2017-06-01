b0466ae720d4b402f74ee70297b11c2f8f7af040
Laurynas Biveinis <laurynas.biveinis@gmail.com>
2017-06-01 16:54:01 +0300

    Fix bugs 76142 / 82480 / 1548597 (InnoDB tablespace import fails when importing table w/ different data directory)
    Importing a transportable tablespace will fail if its table definition
    has data directory specified while the donor table did not have one,
    and vice versa. Fix by relaxing the checks in fil_node_open_file() and
    row_import::match_schema() to allow this particular mismatch while
    still requiring that the rest of the flags match. Do not actually
    patch the tablespace header with the new flag value as InnoDB is
    already making assumptions elsewhere that the data directory flag bits
    may mismatch. Also fix tablespace import diagnostics that in the case
    of flags mismatch printed number of columns, not flags (bug 82480).
    Add a testcase.

--- storage/innobase/row/row0import.cc
+++ storage/innobase/row/row0import.cc
@@ -1319,12 +1319,15 @@ row_import::match_schema(
 	THD*		thd) UNIV_NOTHROW
 {
 	/* Do some simple checks. */
+	const auto relevant_flags = m_flags & ~DICT_TF_MASK_DATA_DIR;
+	const auto relevant_table_flags
+		= m_table->flags & ~DICT_TF_MASK_DATA_DIR;
 
-	if (m_flags != m_table->flags) {
+	if (relevant_flags != relevant_table_flags) {
 		ib_errf(thd, IB_LOG_LEVEL_ERROR, ER_TABLE_SCHEMA_MISMATCH,
-			 "Table flags don't match, server table has 0x%lx"
-			 " and the meta-data file has 0x%lx",
-			 (ulong) m_table->n_cols, (ulong) m_flags);
+			 "Table flags don't match, server table has 0x%x "
+			 "and the meta-data file has 0x%lx",
+			 relevant_table_flags, relevant_flags);
 
 		return(DB_ERROR);
 	} else if (m_table->n_cols != m_n_cols) {
@@ -3930,7 +3933,8 @@ row_import_for_mysql(
 		btr_sdi_create_index(table->space, true);
 		dict_mutex_exit_for_mysql();
 	} else {
-		ut_ad(space->flags == space_flags_from_disk);
+		ut_ad((space->flags & ~FSP_FLAGS_MASK_DATA_DIR)
+		      == (space_flags_from_disk & ~FSP_FLAGS_MASK_DATA_DIR));
 	}
 
 	if (dict_table_is_encrypted(table)) {
