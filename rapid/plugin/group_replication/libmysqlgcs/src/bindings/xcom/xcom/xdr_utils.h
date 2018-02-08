31c572c1ce236743c4c30d6664600c901bb2e79f
Zsolt Parragi <zsolt.parragi@cancellar.hu>
2018-02-08 13:04:03 +0100

    PS-3767: Added clang 4/5 warnings fixes for PS 5.7
    Based on 320473c2c2307d155feaa81e90c91b329e6c74eb

--- rapid/plugin/group_replication/libmysqlgcs/src/bindings/xcom/xcom/xdr_utils.h
+++ rapid/plugin/group_replication/libmysqlgcs/src/bindings/xcom/xcom/xdr_utils.h
@@ -32,7 +32,9 @@ extern "C" {
 /**
     Initialize an array
  */
-#define def_init_xdr_array(name) static inline void init_##name##_array(name##_array *x)
+#define def_init_xdr_array(name) \
+  MY_ATTRIBUTE((unused))         \
+  static inline void init_##name##_array(name##_array *x)
 #define init_xdr_array(name)                                                 \
   def_init_xdr_array(name) {                                                 \
     x->name##_array_len = 2;                                                 \
@@ -42,7 +44,9 @@ extern "C" {
 /**
     Free the contents of an array
  */
-#define def_free_xdr_array(name) static inline void free_##name##_array(name##_array *x)
+#define def_free_xdr_array(name) \
+  MY_ATTRIBUTE((unused))         \
+  static inline void free_##name##_array(name##_array *x)
 #define free_xdr_array(name)   \
   def_free_xdr_array(name) {   \
     free(x->name##_array_val); \
@@ -72,6 +76,7 @@ extern "C" {
     Define a set function for an array
  */
 #define def_set_xdr_array(name) \
+  MY_ATTRIBUTE((unused))        \
   static inline void set_##name(name##_array *x, name a, u_int n)
 #define set_xdr_array(name)          \
   def_set_xdr_array(name) {          \
@@ -83,7 +88,9 @@ extern "C" {
 /**
     Define a get function for an array
  */
-#define def_get_xdr_array(name) static inline name get_##name(name##_array *x, u_int n)
+#define def_get_xdr_array(name) \
+  MY_ATTRIBUTE((unused))        \
+  static inline name get_##name(name##_array *x, u_int n)
 #define get_xdr_array(name)          \
   def_get_xdr_array(name) {          \
     expand_xdr_array(name);          \
@@ -95,6 +102,7 @@ extern "C" {
     Define a function to clone an array
  */
 #define def_clone_xdr_array(name) \
+  MY_ATTRIBUTE((unused)) \
   static inline name##_array clone_##name##_array(name##_array x)
 #define clone_xdr_array(name)                                                \
   def_clone_xdr_array(name) {                                                \
