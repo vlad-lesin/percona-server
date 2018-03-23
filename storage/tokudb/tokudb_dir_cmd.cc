9cb62edddf211d1dd02831feb3341dc1e65f8bdf
George O. Lorch III <george.lorch@percona.com>
2018-03-23 11:02:00 -0700

    TDB-170 : 8.0 : 8.0 changed Table_ident interface used by tokudb_dir_cmd
    - Changed use of Table_ident to fit new interface.

--- storage/tokudb/tokudb_dir_cmd.cc
+++ storage/tokudb/tokudb_dir_cmd.cc
@@ -49,7 +49,7 @@ static int MDL_and_TDC(THD *thd,
     db_arg.length = strlen(db);;
     table_arg.str = const_cast<char *>(table);
     table_arg.length = strlen(table);
-    Table_ident table_ident(thd, db_arg, table_arg, true);
+    Table_ident table_ident(db_arg, table_arg);
     thd->lex->select_lex->add_table_to_list(
         thd, &table_ident, NULL, 1, TL_UNLOCK, MDL_EXCLUSIVE, 0, 0, 0);
     /* The lock will be released at the end of mysq_execute_command() */
