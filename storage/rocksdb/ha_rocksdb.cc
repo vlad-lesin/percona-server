54d8250f219333b09b222c6ee7dc9b2646f7b9a8
George O. Lorch III <george.lorch@percona.com>
2018-04-12 11:43:14 -0700

    MYR-280 : 8.0 : 8.0 changed return types for XA commit and rollback functions
    - Fixed XA return types and return values.

--- storage/rocksdb/ha_rocksdb.cc
+++ storage/rocksdb/ha_rocksdb.cc
@@ -2922,7 +2922,8 @@ static int rocksdb_prepare(handlerton *const hton, THD *const thd,
  do nothing for prepare/commit by xid
  this is needed to avoid crashes in XA scenarios
 */
-static int rocksdb_commit_by_xid(handlerton *const hton, XID *const xid) {
+static xa_status_code rocksdb_commit_by_xid(handlerton *const hton,
+                                            XID *const xid) {
   DBUG_ENTER_FUNC();
 
   DBUG_ASSERT(hton != nullptr);
@@ -2937,14 +2938,14 @@ static int rocksdb_commit_by_xid(handlerton *const hton, XID *const xid) {
   rocksdb::Transaction *const trx = rdb->GetTransactionByName(name);
 
   if (trx == nullptr) {
-    DBUG_RETURN(HA_EXIT_FAILURE);
+    DBUG_RETURN(XAER_NOTA);
   }
 
   const rocksdb::Status s = trx->Commit();
 
   if (!s.ok()) {
     rdb_log_status_error(s);
-    DBUG_RETURN(HA_EXIT_FAILURE);
+    DBUG_RETURN(XAER_RMERR);
   }
 
   delete trx;
@@ -2952,12 +2953,12 @@ static int rocksdb_commit_by_xid(handlerton *const hton, XID *const xid) {
   // `Add()` is implemented in a thread-safe manner.
   commit_latency_stats->Add(timer.ElapsedNanos() / 1000);
 
-  DBUG_RETURN(HA_EXIT_SUCCESS);
+  DBUG_RETURN(XA_OK);
 }
 
-static int rocksdb_rollback_by_xid(handlerton *const hton
-                                   MY_ATTRIBUTE((__unused__)),
-                                   XID *const xid) {
+static xa_status_code
+rocksdb_rollback_by_xid(handlerton *const hton MY_ATTRIBUTE((__unused__)),
+                        XID *const xid) {
   DBUG_ENTER_FUNC();
 
   DBUG_ASSERT(hton != nullptr);
@@ -2969,19 +2970,19 @@ static int rocksdb_rollback_by_xid(handlerton *const hton
   rocksdb::Transaction *const trx = rdb->GetTransactionByName(name);
 
   if (trx == nullptr) {
-    DBUG_RETURN(HA_EXIT_FAILURE);
+    DBUG_RETURN(XAER_NOTA);
   }
 
   const rocksdb::Status s = trx->Rollback();
 
   if (!s.ok()) {
     rdb_log_status_error(s);
-    DBUG_RETURN(HA_EXIT_FAILURE);
+    DBUG_RETURN(XAER_RMERR);
   }
 
   delete trx;
 
-  DBUG_RETURN(HA_EXIT_SUCCESS);
+  DBUG_RETURN(XA_OK);
 }
 
 /**
