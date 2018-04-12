fba8d4c18544a835ad52c9bf414031d99bf878ae
George O. Lorch III <george.lorch@percona.com>
2018-04-12 12:44:15 -0700

    PS-4240 : 8.0 : 8.0 properly exports/exposes thd_binlog_format and thd_binlog_filter_ok via new header
    - Removed local declarations and included correct header

--- storage/rocksdb/ha_rocksdb.cc
+++ storage/rocksdb/ha_rocksdb.cc
@@ -44,6 +44,7 @@
 #include "sql/sql_audit.h"
 #include "sql/sql_lex.h"
 #include "sql/sql_table.h"
+#include "sql/sql_thd_internal_api.h"
 #include "sql/table.h"
 
 // Both MySQL and RocksDB define the same constant. To avoid compilation errors
@@ -76,23 +77,6 @@
 #include "./rdb_mutex_wrapper.h"
 #include "./rdb_psi.h"
 #include "./rdb_threads.h"
-
-// Internal MySQL APIs not exposed in any header.
-extern "C" {
-/**
- *   Get the user thread's binary logging format
- *   @param thd  user thread
- *   @return Value to be used as index into the binlog_format_names array
-*/
-int thd_binlog_format(const MYSQL_THD thd);
-
-/**
- *   Check if binary logging is filtered for thread's current db.
- *   @param  thd   Thread handle
- *   @retval 1 the query is not filtered, 0 otherwise.
-*/
-bool thd_binlog_filter_ok(const MYSQL_THD thd);
-}
 
 // MySQL 8.0 logger service interface
 static SERVICE_TYPE(registry) *reg_srv= nullptr;
